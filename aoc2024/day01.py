92b5d162d407c1ae663db89be1c25aba64727b18
Roy Straub <roy@codecraftr.nl>
2024-12-03 20:42:45 +0100

    Calculates total distance by zipping the lists

--- aoc2024/day01.py
+++ aoc2024/day01.py
@@ -4,9 +4,9 @@ import sys
 
 def total_distance(path: str) -> int:
     contents = read_contents(path)
-    lists = parse_lists(contents)
+    left_list, right_list = parse_lists(contents)
 
-    return calculate_total_distance(lists)
+    return calculate_total_distance(left_list, right_list)
 
 
 def read_contents(path: str) -> list[str]:
@@ -14,7 +14,7 @@ def read_contents(path: str) -> list[str]:
     with open(path, "r") as f:
         lines = f.readlines()
         for line in lines:
-            contents.append(line.replace("\n", ""))
+            contents.append(line.strip())
     return contents
 
 
@@ -23,14 +23,18 @@ def parse_lists(input):
     right_list = []
 
     for line in input:
-        left_list.append(line.split("   ")[0])
-        right_list.append(line.split("   ")[1])
+        left_list.append(int(line.split("   ")[0]))
+        right_list.append(int(line.split("   ")[1]))
 
     return (left_list, right_list)
 
 
-def calculate_total_distance(lists):
-    return -1
+def calculate_total_distance(left_list, right_list):
+    zipped = list(zip(sorted(left_list), sorted(right_list)))
+
+    distances = [abs(right - left) for left, right in zipped]
+
+    return sum(distances)
 
 
 if __name__ == "__main__":
--- aoc2024/tests/test_day01.py
+++ aoc2024/tests/test_day01.py
@@ -2,5 +2,5 @@ from aoc2024.day01 import total_distance
 
 
 def test_day_one():
-    distance = total_distance("aoc2024/tests/day01.txt")
+    distance = total_distance("./aoc2024/tests/day01.txt")
     assert distance == 11
