0065a2e1683bcd5814595aba79b908246b12a603
Roy Straub <roy@codecraftr.nl>
2024-12-04 21:06:30 +0100

    Calculates total similarity score (Counter to the rescue)

--- aoc2024/day01.py
+++ aoc2024/day01.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def total_distance(path: str) -> int:
     contents = read_contents(path)
     left_list, right_list = parse_lists(contents)
@@ -33,6 +36,26 @@ def calculate_total_distance(left_list, right_list):
     return sum(distances)
 
 
+def total_similarity(path: str) -> int:
+    contents = read_contents(path)
+    left_list, right_list = parse_lists(contents)
+
+    return calculate_total_similarity(left_list, right_list)
+
+
+def calculate_total_similarity(left_list, right_list):
+    counted_right = Counter(right_list)
+
+    similarities = []
+
+    for num in left_list:
+        occurrences = int(counted_right.get(num) or 0)
+        similarities.append(num * occurrences)
+
+    return sum(similarities)
+
+
 if __name__ == "__main__":
     file_path = "aoc2024/input/day01.txt"
     print(total_distance(file_path))
+    print(total_similarity(file_path))
--- aoc2024/tests/test_day01.py
+++ aoc2024/tests/test_day01.py
@@ -1,6 +1,11 @@
-from aoc2024.day01 import total_distance
+from aoc2024.day01 import total_distance, total_similarity
 
 
-def test_day_one():
+def test_distance_score():
     distance = total_distance("./aoc2024/tests/day01.txt")
     assert distance == 11
+
+
+def test_similarity_score():
+    similarity = total_similarity("./aoc2024/tests/day01.txt")
+    assert similarity == 31
