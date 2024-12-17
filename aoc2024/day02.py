97f1f7bcc39d7e447d37b5b5f3d8dba567b95203
Roy Straub <roy.straub@ns.nl>
2024-12-17 22:01:49 +0100

    Calculate whether a report is safe or unsafe

--- aoc2024/day02.py
+++ aoc2024/day02.py
@@ -1,3 +1,19 @@
 def calculate_number_of_safe_reports(input: str) -> int:
     print(input)
     return -1
+
+def report_is_safe(report: tuple[int, ...]) -> bool:
+    # Builds combinations
+    left_levels = report[:-1]
+    right_levels = report[1:]
+    combinations = zip(left_levels, right_levels)
+
+    diffs = [right - left for left, right in combinations]
+    within_safe_range = all(in_safe_range(diff) for diff in diffs)
+    all_increasing = all(diff > 0 for diff in diffs)
+    all_decreasing = all(diff < 0 for diff in diffs)
+
+    return within_safe_range and (all_increasing or all_decreasing)
+
+def in_safe_range(diff: int) -> bool:
+    return 1 <= abs(diff) <= 3
--- aoc2024/tests/test_day02.py
+++ aoc2024/tests/test_day02.py
@@ -1,4 +1,6 @@
-from aoc2024.day02 import calculate_number_of_safe_reports
+import pytest
+
+from aoc2024.day02 import calculate_number_of_safe_reports, report_is_safe
 
 
 def test_calculate_number_of_safe_reports():
@@ -13,3 +15,20 @@ def test_calculate_number_of_safe_reports():
 
     assert calculate_number_of_safe_reports(reports) == 2
 
+
+@pytest.mark.parametrize("report", [(7, 6, 4, 2, 1), (1, 3, 6, 7, 9)])
+def test_report_is_safe_when_level_diffs_between_one_and_three(report: tuple[int, ...]):
+    assert report_is_safe(report)
+
+
+@pytest.mark.parametrize("report", [(1, 2, 7, 8, 9), (9, 7, 6, 2, 1)])
+def test_report_is_unsafe_when_level_diffs_exceeds_three(report: tuple[int, ...]):
+    assert not report_is_safe(report)
+
+
+def test_report_is_unsafe_when_level_diffs_increase_and_decrease():
+    assert not report_is_safe((1, 3, 2, 4, 5))
+
+
+def test_report_is_unsafe_when_level_diffs_are_stable():
+    assert not report_is_safe((8, 6, 4, 4, 1))
